3c8bc370518e0a8a960083aee0d1064d7d68dca3
mk2 <lycaon@hotmail.co.jp>
2012-01-14 21:53:41 +0900

    reconstruction

--- src/com/Megaloworks/GSDistribute/support/UpdateProgressListener.java
+++
@@ -1,9 +0,0 @@
-package com.Megaloworks.GSDistribute.support;
-
-public interface UpdateProgressListener {
-
-	public void updateProgressBar(int cnt);
-
-	public void updateProgressLbl(String str);
-
-}
